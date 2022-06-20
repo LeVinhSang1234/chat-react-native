09397b800b20744cb4e5c1b18ef052e3103a4819
SangLv <lsang2884@gmail.com>
2022-06-21 01:29:19 +0700

    fix freeze

--- packages/ChildrenFreeze/index.tsx
+++ packages/ChildrenFreeze/index.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {Component} from 'react';
 
 class ChildrenFreeze extends Component<any> {
   shouldComponentUpdate(nProps: any) {
     const {children, ...p} = this.props;
-    return Object.keys(p || {}).some(e => this.props[e] !== nProps[e]);
+    const arrayP = Object.keys(p);
+    return (
+      arrayP.length > 0 && Object.keys(p).some(e => this.props[e] !== nProps[e])
+    );
   }
 
   render() {
